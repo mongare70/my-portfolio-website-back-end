515ec48f363d450f781e15ee8d5e635d4247c2c9
Hillary Mongare <hillarymongare70@gmail.com>
2022-02-23 08:27:44 +0300

    Added test settings

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,9 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
+#used in a testing environment
+app.config['TESTING'] = False 
+
 CORS(app)
 mail = Mail(app)
 
